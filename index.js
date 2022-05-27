7ed6402e0684da064051f8cf717691323cc11406
qshurick <shu.rick.ifmo@gmail.com>
2022-05-27 16:10:41 +0000

    Add script runner

---
+++ index.js
@@ -0,0 +1,23 @@
+const readline = require('readline');
+const { fixBoth } = require('./src/main');
+
+const stuff = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false,
+});
+
+const paths = [];
+
+stuff.on('line', line => {
+    paths.push(line);
+    if (paths.length == 2) {
+        const adjustedPaths = fixBoth(...paths);
+        console.log();
+        console.log('Adjust your paths accordingly:');
+        console.log(adjustedPaths[0]);
+        console.log(adjustedPaths[1]);
+
+        process.exit();
+    }
+});
